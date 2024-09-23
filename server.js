07e2e6b9ea4211f3e1760770499a4477b1bff03e
John Williams <jrw+3@mit.edu>
2024-09-23 21:35:35 +0000

    styles update

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.get('/test-key', async (req, res) => {
   }
 });
 // Start server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
